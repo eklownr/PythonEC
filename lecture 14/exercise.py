30e65e4090c31145f1a2413a847ee3646e6b29c2
Jonas Jons <jonas.jons@consid.se>
2023-11-09 09:16:54 +0100

    added excercise

---
+++ lecture 14/exercise.py
@@ -0,0 +1,18 @@
+""" 
+DAGENS UPPGIFT:
+
+1. Applicera Map för att konvertera scores till olika betyg! 
+F: 0-49
+E: 50-59 
+D: 60-69
+C: 70-79
+B: 80-89
+A: 90-100
+
+2. Använd filter för att sedan lägga alla Med godkänt betyg (E eller högre) i en lista, och F i en annan
+
+3. Använd reduce för att beräkna medelvärdet av betygen på scoreslistan
+
+"""
+
+scores = [45, 41, 85, 92, 78, 90, 44, 89, 95, 88, 63, 75, 80, 82]
