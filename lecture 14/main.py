389882bdfc165261c63448246c25b99bfe132599
Jonas Jons <jonas.jons@consid.se>
2023-11-09 08:55:02 +0100

    pushed l14

---
+++ lecture 14/main.py
@@ -0,0 +1,23 @@
+from functools import reduce
+
+numbers = [1, 2, 3, 4, 5]
+
+
+def square(x):
+    return x**2
+
+
+squared_numbers2 = list(map(square, numbers))
+squared_numbers = list(map(lambda x: x**2, numbers))
+# Result: squared_numbers = [1, 4, 9, 16, 25]
+
+even_numbers = list(filter(lambda x: x % 2 == 0, numbers))
+# Result: even_numbers = [2, 4]
+
+resulting_sum = reduce(lambda x, y: x + y, numbers)
+# Result: result = 15 (1 + 2 + 3 + 4 + 5)
+
+print(squared_numbers)
+print(squared_numbers2)
+print(even_numbers)
+print(resulting_sum)
