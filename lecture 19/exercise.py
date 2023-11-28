d546ad33900e2a91b6b865c5e56f11b9b51bb2ac
Jonas Jons <jonas.jons@consid.se>
2023-11-28 10:04:02 +0100

    forgotten files

---
+++ lecture 19/exercise.py
@@ -0,0 +1,9 @@
+"""
+Todays exercise is a bit more open-ended,
+
+your assignment is to continue working on the local_file_db
+and implement methods for updating and deleting records.
+
+If you have time also implement: Data visualization (matplotlib) and error-handling
+
+"""
